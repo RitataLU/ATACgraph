77ac875d87d87987968a1acb1e7a71ad64503733
RitataLU <38308132+RitataLU@users.noreply.github.com>
2019-10-05 14:15:56 +0800

    Add files via upload

--- script/callPeak.py
+++ script/callPeak.py
@@ -13,9 +13,9 @@ def main():
 	parser = argparse.ArgumentParser()
 	#1:integration site, 2:whole reads
 	parser.add_argument('-s', dest='separate', help="1: integration site; 2: whole fragment",type=int, default=1)
-	parser.add_argument("-shift",help="shift from integration site",dest='shift',default=10,type=int)
-	parser.add_argument("-ES",help="extend size from integration site",dest='extend',default=20,type=int)
-	parser.add_argument("-bs",help="binsize for bigwig",dest='binsize',default=10,type=int)
+	parser.add_argument("-shift",help="shift size from integration site(bp), default: 10",dest='shift',default=10,type=int)
+	parser.add_argument("-ES",help="extend size from integration site (bp), default: 20",dest='extend',default=20,type=int)
+	parser.add_argument("-bs",help="bin size for bigwig (bp), default: 10",dest='binsize',default=10,type=int)
 	parser.add_argument('input_bam')
 	parser.add_argument('output_name')
 
@@ -39,7 +39,7 @@ def main():
 		subprocess.call('''macs2 callpeak -t %s --nomodel --shift -%s --extsize %s -n %s 2>&1'''%(input_bam,shiftSize,extSize,Outname+'.integ_peak'), shell=True)
 		#Making bigwig/ bamcoverage limit: cannot use minus
                 #subprocess.call('''bamCoverage -b %s -bs 10 --normalizeUsingRPKM --Offset 1 20 -o %s'''%(input_bam, input_bam+'_coverage.bw'),shell=True
-                subprocess.call('''bamCoverage -b %s -bs %s --normalizeUsingRPKM --Offset 1 20 -o %s 2>&1'''%(input_bam, bs, Outname+'.integ_coverage.bw'),shell=True)
+                subprocess.call('''bamCoverage -b %s -bs %s --normalizeUsing RPKM --Offset 1 20 -o %s 2>&1'''%(input_bam, bs, Outname+'_coverage.bw'),shell=True)
         elif(args.separate == 2):
         
             subprocess.call('''samtools index %s'''%(input_bam),shell=True)
@@ -50,7 +50,7 @@ def main():
             #subprocess.call('''macs2 callpeak -t %s --format BAMPE --broad -n %s'''%(input_bam+'_short.bam',input_bam+'_short_peak'), shell=True)
             #Making bam coverage
             #subprocess.call('''bamCoverage -b %s -bs 10 --normalizeUsingRPKM --Offset 1 20 -o %s'''%(input_bam, input_bam+'_coverage.bw'),shell=True)
-            subprocess.call('''bamCoverage -b %s -bs 10 --normalizeUsingRPKM -e -o %s 2>&1'''%(input_bam, Outname+'_coverage.bw'),shell=True)
+            subprocess.call('''bamCoverage -b %s -bs 10 --normalizeUsing RPKM -e -o %s 2>&1'''%(input_bam, Outname+'_coverage.bw'),shell=True)
             #subprocess.call('''bamCoverage -b %s -bs 10 --normalizeUsingRPKM -e -o %s'''%(input_bam+'_short.bam', input_bam+'_short_coverage.bw'),shell=True)
 	
 	else:
